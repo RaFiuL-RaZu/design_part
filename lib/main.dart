3883692999f4735e59d3dbea7445978726db144c
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-13 19:13:25 +0600

    innit commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: HomeScren(),
+    );
+  }
+
+}
+class HomeScren extends StatelessWidget{
+  const HomeScren({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Normal Design"),
+      ),
+    );
+  }
+
+}
