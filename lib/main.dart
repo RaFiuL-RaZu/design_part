219c1beff911f542558d6da1fafb31620aea9b35
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-14 19:02:53 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -40,39 +40,15 @@ class HomeScren extends StatelessWidget {
         title: Text("Home Design"),
       ),
       drawer: Drawer(),
-      body: GridView.builder(
-        itemCount: personInfo.length,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-              childAspectRatio: 0.70,
-              crossAxisSpacing: 15,
-              mainAxisSpacing: 15,),
-          itemBuilder: (context, index) {
-            return Card(
-              color: Colors.grey,
-              child: Column(
-                children: [
-                  Image.network(
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",
-                    height: 150,
-                    width: double.maxFinite,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Name :${personInfo[index]["name"]}"),
-                        Text("Dep :${personInfo[index]["dept"]}",maxLines: 1,overflow: TextOverflow.ellipsis,),
-                        Text("Roll:${personInfo[index]["Roll"]}"),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            );
-          }),
+      body:Column(
+        children: [
+          Container(
+            height: 150,
+            width: double.infinity,
+            color: Colors.grey,
+          )
+        ],
+      )
     );
   }
 }
