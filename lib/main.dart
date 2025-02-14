31812d8acbc743e16d984872b43d7f3d24661767
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-14 19:08:25 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -48,41 +48,44 @@ class HomeScren extends StatelessWidget {
               width: double.infinity,
               color: Colors.grey,
             ),
-            GridView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: personInfo.length,
-                gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  childAspectRatio: 0.70,
-                  crossAxisSpacing: 15,
-                  mainAxisSpacing: 15,),
-                itemBuilder: (context, index) {
-                  return Card(
-                    color: Colors.grey,
-                    child: Column(
-                      children: [
-                        Image.network(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",
-                          height: 150,
-                          width: double.maxFinite,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Name :${personInfo[index]["name"]}",maxLines: 1,overflow: TextOverflow.ellipsis,),
-                              Text("Dep :${personInfo[index]["dept"]}",maxLines: 1,overflow: TextOverflow.ellipsis,),
-                              Text("Roll:${personInfo[index]["Roll"]}"),
-                            ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: personInfo.length,
+                  gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 0.65,
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 15,),
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: Colors.grey,
+                      child: Column(
+                        children: [
+                          Image.network(
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",
+                            height: 150,
+                            width: double.maxFinite,
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                }),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Name :${personInfo[index]["name"]}",maxLines: 1,overflow: TextOverflow.ellipsis,),
+                                Text("Dep :${personInfo[index]["dept"]}",maxLines: 1,overflow: TextOverflow.ellipsis,),
+                                Text("Roll:${personInfo[index]["Roll"]}"),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
           ],
         ),
       ),
