4ed9eda389037d6adb6f03e7b9deb958f4987c29
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-14 18:20:32 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,19 @@ class MyApp extends StatelessWidget{
 
 }
 class HomeScren extends StatelessWidget{
-  const HomeScren({Key? key}) : super(key: key);
+   HomeScren({Key? key}) : super(key: key);
+
+   List personInfo=[
+     {"name":"RaFiuL RaZu","dept":"Computer Technology","Roll":"102621"},
+     {"name":"RoBiuL SaZu","dept":"Computer Technology","Roll":"102621"},
+     {"name":"RaiHaN RaZu","dept":"Computer Technology","Roll":"102621"},
+     {"name":"RaFi IslaM","dept":"Computer Technology","Roll":"102621"},
+     {"name":"RaFiZ Hasan","dept":"Computer Technology","Roll":"102621"},
+     {"name":"SaFkaT SHaFin","dept":"Computer Technology","Roll":"102621"},
+     {"name":"RaFiuL RaZu","dept":"Computer Technology","Roll":"102621"},
+     {"name":"RaFiuL RaZu","dept":"Computer Technology","Roll":"102621"},
+     {"name":"RaFiuL RaZu","dept":"Computer Technology","Roll":"102621"},
+   ];
 
 
   @override
@@ -32,7 +44,8 @@ class HomeScren extends StatelessWidget{
       ),
       drawer: Drawer(),
      body: GridView.builder(
-       itemCount: 10,
+
+       itemCount:personInfo.length,
          gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
              crossAxisCount: 3,
          childAspectRatio: 0.75,
@@ -40,7 +53,28 @@ class HomeScren extends StatelessWidget{
            mainAxisSpacing: 15,
          ),
          itemBuilder:(context,index){
-           return  ContactCard();
+           return Card(
+             child: Container(
+               height: 100,
+               width: 100,
+               color: Colors.amber,
+               child: Column(
+                 children: [
+                   Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",height: 150,width: double.infinity,),
+                   Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: Column(
+                       children: [
+                         Text("Name:${personInfo[index]["name"]),
+                         Text("Dep :${personInfo[index]["dept"]),
+                          Text("Roll :${personInfo[index]["Roll"]),
+                       ],
+                     ),
+                   )
+                 ],
+               ),
+             ),
+           );
          }),
     );
   }
