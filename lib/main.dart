c840f7127b39ee25937a991b785f0a144d917304
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-14 17:19:24 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class HomeScren extends StatelessWidget{
              color: Colors.amber,
              child: Column(
                children: [
-                 Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s"),
+                 Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",height: 150,width: double.infinity,),
                  Text("RaFiuL RaZu"),
                  Text("Computer Enginner"),
                ],
