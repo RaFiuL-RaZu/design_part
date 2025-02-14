d29d36fe6ffc0d7df791f034006b902a73f7f80a
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-14 17:34:40 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,112 @@ class HomeScren extends StatelessWidget{
            childAspectRatio: 0.75
          ),
        children: [
-         Padding(
-           padding: const EdgeInsets.all(8.0),
+         Card(
+           child: Container(
+             height: 100,
+             width: 100,
+             color: Colors.amber,
+             child: Column(
+               children: [
+                 Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",height: 150,width: double.infinity,),
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Column(
+                     children: [
+                       Text("RaFiuL RaZu"),
+                       Text("Computer Enginner"),
+                     ],
+                   ),
+                 )
+               ],
+             ),
+           ),
+         ),
+         Card(
+           child: Container(
+             height: 100,
+             width: 100,
+             color: Colors.amber,
+             child: Column(
+               children: [
+                 Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",height: 150,width: double.infinity,),
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Column(
+                     children: [
+                       Text("RaFiuL RaZu"),
+                       Text("Computer Enginner"),
+                     ],
+                   ),
+                 )
+               ],
+             ),
+           ),
+         ),
+         Card(
+           child: Container(
+             height: 100,
+             width: 100,
+             color: Colors.amber,
+             child: Column(
+               children: [
+                 Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",height: 150,width: double.infinity,),
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Column(
+                     children: [
+                       Text("RaFiuL RaZu"),
+                       Text("Computer Enginner"),
+                     ],
+                   ),
+                 )
+               ],
+             ),
+           ),
+         ),
+         Card(
+           child: Container(
+             height: 100,
+             width: 100,
+             color: Colors.amber,
+             child: Column(
+               children: [
+                 Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",height: 150,width: double.infinity,),
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Column(
+                     children: [
+                       Text("RaFiuL RaZu"),
+                       Text("Computer Enginner"),
+                     ],
+                   ),
+                 )
+               ],
+             ),
+           ),
+         ),
+         Card(
+           child: Container(
+             height: 100,
+             width: 100,
+             color: Colors.amber,
+             child: Column(
+               children: [
+                 Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",height: 150,width: double.infinity,),
+                 Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Column(
+                     children: [
+                       Text("RaFiuL RaZu"),
+                       Text("Computer Enginner"),
+                     ],
+                   ),
+                 )
+               ],
+             ),
+           ),
+         ),
+         Card(
            child: Container(
              height: 100,
              width: 100,
