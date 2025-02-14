0d2e7f782a2e344665b1b9002a532c90ee787872
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-14 19:07:44 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -40,15 +40,52 @@ class HomeScren extends StatelessWidget {
         title: Text("Home Design"),
       ),
       drawer: Drawer(),
-      body:Column(
-        children: [
-          Container(
-            height: 150,
-            width: double.infinity,
-            color: Colors.grey,
-          )
-        ],
-      )
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              width: double.infinity,
+              color: Colors.grey,
+            ),
+            GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: personInfo.length,
+                gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 0.70,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,),
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.grey,
+                    child: Column(
+                      children: [
+                        Image.network(
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp57OH7-b1dARGc3vujs7992FaDDvo-2w9Rw&s",
+                          height: 150,
+                          width: double.maxFinite,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Name :${personInfo[index]["name"]}",maxLines: 1,overflow: TextOverflow.ellipsis,),
+                              Text("Dep :${personInfo[index]["dept"]}",maxLines: 1,overflow: TextOverflow.ellipsis,),
+                              Text("Roll:${personInfo[index]["Roll"]}"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+          ],
+        ),
+      ),
     );
   }
 }
